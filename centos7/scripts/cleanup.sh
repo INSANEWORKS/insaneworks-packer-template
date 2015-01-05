6a3a6e7ff5fea0a8378b0860b6b4559f6b0e9f57
Kota Shiratsuka <kota@insaneworks.co.jp>
2015-01-06 01:19:46 +0900

    v20150106
    CentOS6:  Important: RHSA-2014:1997-1 Important: kernel security and bug fix update
    CentOS7:  Important: RHSA-2014:2010-1 Important: kernel security update

--- centos7/scripts/cleanup.sh
+++ centos7/scripts/cleanup.sh
@@ -1,4 +1,29 @@
-rm -f /var/lib/dhclient/dhclient-*.leases
+# ref https://github.com/box-cutter/centos-vm/
+/bin/systemctl stop NetworkManager.service
+for ifcfg in `ls /etc/sysconfig/network-scripts/ifcfg-* | grep -v ifcfg-lo` ; do
+  rm -f $ifcfg
+done
+
+cat <<EOF | cat >> /etc/rc.d/rc.local
+LANG=C
+
+for con in \`nmcli -t -f uuid con\`; do
+  if [ "\$con" != "" ]; then
+    nmcli con del \$con
+  fi
+done
+
+gwdev=\`nmcli dev | grep ethernet | egrep -v 'unmanaged' | head -n 1 | awk '{print \$1}'\`
+if [ "\$gwdev" != "" ]; then
+  nmcli c add type eth ifname \$gwdev con-name \$gwdev
+fi
+
+chmod -x /etc/rc.d/rc.local
+EOF
+
+chmod +x /etc/rc.d/rc.local
+
 rm -f /etc/ssh/ssh_host_*
+rm -f /var/lib/NetworkManager/*
 rm -rf /tmp/*
 yum -y clean all
