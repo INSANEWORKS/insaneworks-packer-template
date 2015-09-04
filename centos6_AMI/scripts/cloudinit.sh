4aa167799324f00ca227251ee0586ed141f4bc0d
Kota Shiratsuka <kota@insaneworks.co.jp>
2015-09-04 17:18:21 +0900

    epel-release include base repo

--- centos6_AMI/scripts/cloudinit.sh
+++ centos6_AMI/scripts/cloudinit.sh
@@ -1,5 +1,5 @@
 # Install cloud-init
-yum -y localinstall http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+yum -y install epel-release
 yum -y install cloud-init
 sed -i 's/disable_root.*/disable_root: 0/' /etc/cloud/cloud.cfg
 sed -i 's/^.*requiretty/#Defaults requiretty/' /etc/sudoers
