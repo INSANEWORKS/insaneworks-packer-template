7fdda8397bf49e35768de824160cd043439f1626
Kota Shiratsuka <kota@insaneworks.co.jp>
2015-11-11 23:28:06 +0900

    VMware Fusion 8 Compatible (force install)

--- centos7/scripts/vmtools.sh
+++ centos7/scripts/vmtools.sh
@@ -14,7 +14,7 @@ mount -o loop /home/vagrant/linux.iso /mnt/cdrom
 tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp
 cd /tmp/vmware-tools-distrib
 yum -y install net-tools
-/tmp/vmware-tools-distrib/vmware-install.pl -d
+/tmp/vmware-tools-distrib/vmware-install.pl -d -f
 umount /mnt/cdrom
 rm -rf /tmp/vmware-tools-distrib
 rm -rf /home/vagrant/linux.iso
