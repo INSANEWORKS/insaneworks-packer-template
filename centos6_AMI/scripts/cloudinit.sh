e6f6ad04c256bf388179744221b1fc6e4ec1bfbf
Kota Shiratsuka <kota@insaneworks.co.jp>
2015-12-04 18:59:48 +0900

    Support CentOS Linux 6 x86_64 HVM EBS 20150928_01

--- centos6_AMI/scripts/cloudinit.sh
+++ centos6_AMI/scripts/cloudinit.sh
@@ -1,5 +1,6 @@
 # Install cloud-init
 yum -y install epel-release
-yum -y install cloud-init
+yum -y install cloud-init cloud-utils dracut-modules-growroot
 sed -i 's/disable_root.*/disable_root: 0/' /etc/cloud/cloud.cfg
+sed -i 's/\(resize_rootfs_tmp.*\)/\1\nresize_rootfs: true/' /etc/cloud/cloud.cfg
 sed -i 's/^.*requiretty/#Defaults requiretty/' /etc/sudoers
