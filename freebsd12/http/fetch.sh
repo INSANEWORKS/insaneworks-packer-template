8b4aaaed4426f6f1fa248cbec39ff0692d45a6e4
Kota Shiratsuka <kota@insaneworks.co.jp>
2020-02-21 09:40:48 +0900

    remove lib32

--- freebsd12/http/fetch.sh
+++ freebsd12/http/fetch.sh
@@ -1,5 +1,5 @@
 export BSDINSTALL_DISTSITE="ftp://ftp.freebsd.org/pub/FreeBSD/releases/amd64/12.1-RELEASE"
-export DISTRIBUTIONS="base.txz lib32.txz kernel.txz"
+export DISTRIBUTIONS="base.txz kernel.txz"
 export BSDINSTALL_DISTDIR="/media"
 mdmfs -s 500m md0 /media
 bsdinstall distfetch
