83fc3348dcb3912ad8e610fc3c0108fa791a47fb
Kota Shiratsuka <kota@insaneworks.co.jp>
2020-10-28 21:06:11 +0900

    v20201028
    * FreeBSD 12.2-RELEASE

--- freebsd12/http/fetch.sh
+++ freebsd12/http/fetch.sh
@@ -1,4 +1,4 @@
-export BSDINSTALL_DISTSITE="ftp://ftp.freebsd.org/pub/FreeBSD/releases/amd64/12.1-RELEASE"
+export BSDINSTALL_DISTSITE="ftp://ftp.freebsd.org/pub/FreeBSD/releases/amd64/12.2-RELEASE"
 export DISTRIBUTIONS="base.txz kernel.txz"
 export BSDINSTALL_DISTDIR="/media"
 mdmfs -s 500m md0 /media
