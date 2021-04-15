035796a6c52bcb85db524d705fae6d74f642246a
Kota Shiratsuka <kota@insaneworks.co.jp>
2021-04-15 17:59:12 +0900

    v20210415
    * FreeBSD 13.0-RELEASE-p0

---
+++ freebsd13/http/fetch.sh
@@ -0,0 +1,5 @@
+export BSDINSTALL_DISTSITE="ftp://ftp.freebsd.org/pub/FreeBSD/releases/amd64/13.0-RELEASE"
+export DISTRIBUTIONS="base.txz kernel.txz"
+export BSDINSTALL_DISTDIR="/media"
+mdmfs -s 500m md0 /media
+bsdinstall distfetch
