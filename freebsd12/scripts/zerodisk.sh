aae62d4f49b8184daed131e15baad54b198bd0d7
Kota Shiratsuka <kota@insaneworks.co.jp>
2022-08-10 01:37:15 +0900

    Drop x86_64 architecture distribution

--- freebsd12/scripts/zerodisk.sh
+++
@@ -1,4 +0,0 @@
-zfs set compression=off zroot/ROOT/default
-dd if=/dev/zero of=/EMPTY bs=1M
-rm /EMPTY
-zfs set compression=lz4 zroot/ROOT/default
--- freebsd13/scripts/cleanup.sh
+++
@@ -1,4 +0,0 @@
-rm -f /etc/ssh/ssh_host_*
-rm -rf /var/db/freebsd-update/*
-rm -rf /tmp/*
-pkg clean -a -y
