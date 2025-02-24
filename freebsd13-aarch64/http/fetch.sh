400f794cd134707462de7a98ae96d6e311620460
Kota Shiratsuka <kota@insaneworks.co.jp>
2025-02-24 21:24:51 +0900

    v20250224
    * Parallels Desktop 20.2.3
    * FreeBSD 14.2-RELEASE-p2
    * FreeBSD 13.4-RELEASE-p4
    * AlmaLinux 9.5

--- freebsd13-aarch64/http/fetch.sh
+++ freebsd13-aarch64/http/fetch.sh
@@ -1,4 +1,4 @@
-export BSDINSTALL_DISTSITE="ftp://ftp.freebsd.org/pub/FreeBSD/releases/arm64/13.3-RELEASE"
+export BSDINSTALL_DISTSITE="ftp://ftp.freebsd.org/pub/FreeBSD/releases/arm64/13.4-RELEASE"
 export DISTRIBUTIONS="base.txz kernel.txz"
 export BSDINSTALL_DISTDIR="/media"
 mdmfs -s 500m md0 /media
