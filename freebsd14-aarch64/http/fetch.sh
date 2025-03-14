811cff68feb88ceeb76ff17e064ad70249102b54
Kota Shiratsuka <kota@insaneworks.co.jp>
2025-03-15 00:32:35 +0900

    v20250608
    * FreeBSD 14.3-RELEASE
    * AlmaLinux 9.6

--- freebsd14-aarch64/http/fetch.sh
+++ freebsd14-aarch64/http/fetch.sh
@@ -1,4 +1,4 @@
-export BSDINSTALL_DISTSITE="ftp://ftp.freebsd.org/pub/FreeBSD/releases/arm64/14.2-RELEASE"
+export BSDINSTALL_DISTSITE="ftp://ftp.freebsd.org/pub/FreeBSD/releases/arm64/14.3-RELEASE"
 export DISTRIBUTIONS="base.txz kernel.txz"
 export BSDINSTALL_DISTDIR="/media"
 mdmfs -s 500m md0 /media
